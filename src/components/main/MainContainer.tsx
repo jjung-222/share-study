7fecb3474090d178467b521290cbdd28f068d641
jjung-222 <lyj7877@naver.com>
2024-09-11 10:12:28 +0900

    style: layout change

--- src/components/main/MainContainer.tsx
+++ src/components/main/MainContainer.tsx
@@ -11,8 +11,16 @@ const MainContainer: React.FC = () => {
           <div className="border border-gray-300 p-4">
             <Calendar />
           </div>
-          <div className="flex flex-col items-center justify-between border border-gray-300 p-4 text-center">
-            <p className="text-xl font-semibold mb-2 flex-grow-0 flex-shrink-0">UPCOMING</p>
+          <div className="flex flex-col items-center flex justify-between border border-gray-300 p-0 text-center ">
+            <div className='grid grid-cols-3 flex items-center justify-center' style={{ gridTemplateColumns: '1fr 1fr 1fr' }}>
+            <div className="border border-gray-300"></div>
+            <div className="border border-gray-300 text-center"><p className="text-xl font-semibold flex-grow-0 flex-shrink-0">UPCOMING</p></div>
+            <div className="border border-gray-300 flex justify-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+              </svg>
+            </div>
+            </div>
             <div className="container p-4 flex-grow border border-gray-300">
               <p>Content</p>
             </div>
